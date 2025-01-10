af6fe0286a708ba7c54b52af8baa03354cf22168
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2025-01-10 14:15:24 +0100

    partie 2

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,9 +1,10 @@
 use crate::exponential_fast::exponential_fast;
-use crate::utils::{are_coprime, pgcd};
+use crate::utils::{are_coprime, is_probably_prime, pgcd};
 
 #[test]
 fn test_pgcd() {
     assert_eq!(pgcd(12, 18), 6);
+    assert_eq!(pgcd(15, 8), 1);
     assert_eq!(pgcd(18, 12), 6);
     assert_eq!(pgcd(0, 18), 18);
 }
@@ -16,7 +17,6 @@ fn test_are_co_prime() {
     assert_eq!(are_coprime(12, 39), false);
 }
 
-
 #[test]
 fn test_exponential_fast() {
     assert_eq!(exponential_fast(2, 0), 1);
@@ -24,4 +24,18 @@ fn test_exponential_fast() {
     assert_eq!(exponential_fast(2, 2), 4);
     assert_eq!(exponential_fast(2, 3), 8);
     assert_eq!(exponential_fast(2, 4), 16);
+}
+
+#[test]
+fn test_is_probably_prime() {
+    const PRIME_TO_TEST: [u64; 11] = [3, 5, 7, 11, 13, 19, 241, 599, 1319, 3671, 7879];
+    const NORMAL_TO_TEST: [u64; 7] = [10, 20, 30, 50, 1000, 55555, 45678];
+
+    for &n in &PRIME_TO_TEST {
+        assert_eq!(is_probably_prime(n), true);
+    }
+
+    for &n in &NORMAL_TO_TEST {
+        assert_eq!(is_probably_prime(n), false);
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -22,3 +22,54 @@ pub fn pgcd(a: u64, b: u64) -> u64 {
 pub fn are_coprime(a: u64, b: u64) -> bool {
     pgcd(a, b) == 1
 }
+
+/// Vérifie si un nombre est probablement premier en utilisant les bases 2, 3, 5 et 7
+/// # Arguments
+/// * `n` - Le nombre à tester
+/// # Returns
+/// * `true` si `n` est probablement premier, `false` sinon
+pub fn is_probably_prime(n: u64) -> bool {
+    const BASES: [u64; 4] = [2, 3, 5, 7]; // Bases utilisées pour le test de primalité
+
+    if n < 2 {
+        return false;
+    }
+
+    // Vérifie si n est divisible par une des bases (autre qu'elle-même)
+    for &base in &BASES {
+        if n > base && n % base == 0 {
+            return false;
+        }
+    }
+
+    // Vérifie la condition (base^(n-1) % n == 1) pour chaque base
+    for &base in &BASES {
+        if base < n && exponential_fast_mod(base, n - 1, n) != 1 {
+            return false;
+        }
+    }
+
+    true
+}
+
+/// Calcule l'exponentiation rapide de g^x mod n
+/// # Arguments
+/// * `g` - La base
+/// * `x` - L'exposant
+/// * `modu` - Le modulo
+/// # Returns
+/// * Le résultat de l'exponentiation rapide
+pub fn exponential_fast_mod(g: u64, x: u64, modu: u64) -> u64 {
+    let mut aux: u64 = g % modu; // Base initiale mod n
+    let mut output: u64 = 1;
+    let mut x: u64 = x;
+
+    while x != 0 {
+        if x & 1 == 1 {
+            output = (output * aux) % modu;
+        }
+        x = x >> 1;
+        aux = (aux * aux) % modu;
+    }
+    output
+}
